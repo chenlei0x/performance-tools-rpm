ab68f1258cdf3ffb9618e99aabccf2368af2cd48
Lei Chen <lei.chen@smartx.com>
2022-05-23 07:26:35 +0000

    mbw

---
+++ mbw/build.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+pwd=$PWD
+cd "$(dirname "$0")"
+
+distro=$1
+if [ ! -e ../_docker/$distro/config.sh ]; then
+    echo ../_docker/$distro/config.sh not found
+    echo $0 distro:
+    ls ../_docker
+    exit 1
+fi
+source ../_docker/$distro/config.sh 
+
+rm -rf rpmbuild
+mkdir -p rpmbuild/SOURCES
+mkdir -p rpmbuild/SPECS
+
+cp *.spec rpmbuild/SPECS
+cp mbw-1.5.tar.gz rpmbuild/SOURCES
+
+docker pull $img_tag
+docker run  --rm -v $PWD/rpmbuild:$PWD/rpmbuild  --net=host $img_tag\
+    rpmbuild --define "dist $dist" --define "_topdir $PWD/rpmbuild" -ba $PWD/rpmbuild/SPECS/mbw.spec
